e8b8f937d7d404da204958485f2910d89c0de6ca
Katie Frank <katiexfrank@gmail.com>
2020-08-29 14:54:41 -0500

    add vignette

--- tests/testthat/test-error.R
+++ tests/testthat/test-error.R
@@ -5,4 +5,11 @@ test_that("misuses of scores()", {
                "`marks` must be a numeric or character vector", fixed = TRUE)
   expect_error(scores(c(`100m` = 10.12, LJ = -7), "male", "decathlon"),
                "Invalid entry for `marks`: negative mark(s) not allowed", fixed = TRUE)
+  expect_error(scores(c(`1500m` = "5:20.38"), "male", seconds = "true"),
+               "Invalid entry for `seconds`", fixed = TRUE)
+  expect_error(scores(c(10.12, 7), "male"),
+               "Every element of `marks` must be named if `combined_event` is unspecified",
+               fixed = TRUE)
+  expect_error(scores(c(`100m` = 10.12, lj = 7), "male"),
+               "One or more invalid names for `marks`", fixed = TRUE)
 })
