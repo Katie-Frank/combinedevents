9f821cb87ca22e54aa809d659b6acaa61146aed4
Katie Frank <katiexfrank@gmail.com>
2020-08-31 18:17:13 -0500

    Add package documentation

---
+++ R/combinedevents.R
@@ -0,0 +1,18 @@
+#' combinedevents: Calculate Scores and Marks for Track and Field
+#' Combined Events
+#'
+#' Includes functions to calculate scores and marks for track and
+#' field combined events competitions. The functions are based on the IAAF
+#' Scoring Tables for Combined Events.
+#'
+#' @author **Maintainer**: Katie Frank \email{katiexfrank@gmail.com}
+#'
+#' @seealso Useful links:
+#' \itemize{
+#' \item \url{https://github.com/katie-frank/combinedevents}
+#' \item Report bugs at \url{https://github.com/katie-frank/combinedevents/issues}
+#' }
+#'
+#' @docType package
+#' @name combinedevents
+NULL
